1449c1ffe4e3118fe7ef5279d161c9e1e61c53cf
Pankaj Singh Sirari <pankajsirari222@gmail.com>
2022-09-26 11:30:16 +0530

    initial commit

---
+++ codeforces.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+            long n = sc.nextInt();
+            long m = sc.nextInt();
+            long a = sc.nextInt();
+            long x = 0, y = 0;
+
+            x = n/a;
+            y = m/a;
+
+            if(n%a !=0) x++;
+            if(m%a !=0) y++;
+            System.out.println(x*y);
+
+            // System.out.println((long)(Math.ceil(n/a) * Math.ceil(m/a)));       //One line solution
+           
+        sc.close();
+
+    }
+}
